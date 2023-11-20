b07befd59aa82d4d62aafdf3090092c3f4da638f
EnhancedTarget <levente.kalman922@gmail.com>
2023-11-20 21:54:42 +0000

    w6: switch scenes by pressing "c"

--- Week6Lab/Assets/Scripts/SceneSwitch.cs
+++ Week6Lab/Assets/Scripts/SceneSwitch.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public string SceneName;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
     }
 }
