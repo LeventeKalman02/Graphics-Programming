79e44f34b249e6ef80a05ca08c4256ab5c78bb09
EnhancedTarget <levente.kalman922@gmail.com>
2023-11-20 22:46:40 +0000

    w6: added projectiles shooting from zombie

---
+++ Week6Lab/Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    //declaring rigidbody
+    public Rigidbody projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //this creates the projectile every time the key is pressed
+            Rigidbody clone;
+            clone = Instantiate(projectile, transform.position, transform.rotation);
+
+            clone.velocity = transform.forward * 100;
+
+        }
+    }
+}
