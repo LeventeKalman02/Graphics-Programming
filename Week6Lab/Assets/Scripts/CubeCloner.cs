3b24308c5bec5d7ed6f2a746687b264d435c4252
EnhancedTarget <levente.kalman922@gmail.com>
2023-11-20 22:28:40 +0000

    w6: cloning cubes on keypress

---
+++ Week6Lab/Assets/Scripts/CubeCloner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class CubeCloner : MonoBehaviour
+{
+    public GameObject Cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //statement to spawn a new cube at set coordinates every time the space key is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key was pressed.");
+
+            Instantiate(Cube, new Vector3(0, 1, 0), Quaternion.identity);
+            
+        }
+    }
+}
--- Week6Lab/Assets/Scripts/SphereMovement.cs
+++ Week6Lab/Assets/Scripts/SphereMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShpereMovement : MonoBehaviour
+public class SphereMovement : MonoBehaviour
 {
     //declaring rigidBody
     public Rigidbody rb;
